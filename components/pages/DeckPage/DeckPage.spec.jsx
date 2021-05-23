5e6cc20d6ced98f0eeae2a086a22780967736b70
Sam Boylett <sam.boylett@netsells.co.uk>
2021-05-23 02:35:31 +0100

    main - DeckPage tests

--- components/pages/DeckPage/DeckPage.spec.jsx
+++ components/pages/DeckPage/DeckPage.spec.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
+import { useAsync } from 'react-async'
+import { Loader, Message } from 'semantic-ui-react'
 
 import { DeckPage } from './DeckPage'
 import { DeckTemplate } from '../../templates/DeckTemplate/DeckTemplate'
@@ -10,6 +12,7 @@ describe('DeckPage', () => {
 
     describe('when rendered', () => {
         beforeEach(() => {
+            useAsync.mockReturnValue({})
             wrapper = mount(<DeckPage />)
         })
 
@@ -19,6 +22,28 @@ describe('DeckPage', () => {
 
         test('passes default deck to template', () => {
             expect(wrapper.find(DeckTemplate).prop('deck')).toEqual(getDeck())
+        })
+    })
+
+    describe('when loading', () => {
+        beforeEach(() => {
+            useAsync.mockReturnValue({ isPending: true })
+            wrapper = mount(<DeckPage />)
+        })
+
+        test('renders a Loader', () => {
+            expect(wrapper.find(Loader)).toHaveLength(1)
+        })
+    })
+
+    describe('when errored', () => {
+        beforeEach(() => {
+            useAsync.mockReturnValue({ error: new Error })
+            wrapper = mount(<DeckPage />)
+        })
+
+        test('renders a Message', () => {
+            expect(wrapper.find(Message)).toHaveLength(1)
         })
     })
 })
